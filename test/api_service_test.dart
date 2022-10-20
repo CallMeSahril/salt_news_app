0f76b47c4caf1ed03ba797def04050aecd3622a8
Sahril <103849124+CallMeSahril@users.noreply.github.com>
2022-10-20 17:18:06 +0700

    edit != null jadi isnotempaty

--- test/api_service_test.dart
+++ test/api_service_test.dart
@@ -1,12 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:salt_news_app/data/api/api_service.dart';
+import 'package:salt_news_app/data/model/newsresult_model.dart';
 
 void main() {
   test("Fetch Api", () async {
     bool done = false;
-    var fetch = (await ApiService().topHeadlines()).articles;
+    List<Article> fetch = (await ApiService().topHeadlines()).articles;
 
-    if (fetch != null) {
+    if (fetch.isNotEmpty) {
       done = true;
     } else {}
     expect(done, true);
